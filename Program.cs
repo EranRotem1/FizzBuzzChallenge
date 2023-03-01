e01b50cfc38cceac3beb7a16b167606d0d34242f
Eran Rotem <eranrotem123@gmail.com>
2023-03-01 09:43:35 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        public static void FizzBuzz(int num)
+        {
+            for (int i = 1; i <= num; i++) 
+            {
+                if (i % 5 == 0 && i % 3 == 0) { Console.WriteLine("FizzBuzz"); }
+                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                else Console.WriteLine(i.ToString()) ;
+            }
+        }
+        static void Main(string[] args)
+        {
+            FizzBuzz(15);
+        }
+    }
+}
